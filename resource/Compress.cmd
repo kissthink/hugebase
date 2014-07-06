3fa5586dab95872d748b013fc4947c5434b37db3
txdx <txdx@2361c462-ec5a-1946-8964-ff0cdbb1caab>
2014-07-06 05:53:04 +0000

    git-svn-id: https://svn.mythkast.net/svn/pig3@9 2361c462-ec5a-1946-8964-ff0cdbb1caab

--- resource/Compress.cmd
+++ resource/Compress.cmd
@@ -1,9 +1,9 @@
 -ConsoleImz -imzw wmap wmap.imz
 -ConsoleImz -imzw cloud cloud.imz
 -ConsoleImz -imzw eft eft.imz
-ConsoleImz -imzw smap smap.imz
-ConsoleImz -imzw mmap mmap.imz
+-ConsoleImz -imzw smap smap.imz
+-ConsoleImz -imzw mmap mmap.imz
 -ConsoleImz -imzw title title.imz
--ConsoleImz -imzw head head.imz
+ConsoleImz -imzw head head.imz
 -ConsoleImz -imzw item item.imz
 pause
