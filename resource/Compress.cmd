944eca038acde29807b670b5a8b25957125540d5
cd <cd@2361c462-ec5a-1946-8964-ff0cdbb1caab>
2014-07-07 09:29:12 +0000

    主要是修改菜单，修正一处错误
    git-svn-id: https://svn.mythkast.net/svn/pig3@10 2361c462-ec5a-1946-8964-ff0cdbb1caab

--- resource/Compress.cmd
+++ resource/Compress.cmd
@@ -2,8 +2,8 @@
 -ConsoleImz -imzw cloud cloud.imz
 -ConsoleImz -imzw eft eft.imz
 -ConsoleImz -imzw smap smap.imz
--ConsoleImz -imzw mmap mmap.imz
+ConsoleImz -imzw mmap mmap.imz
 -ConsoleImz -imzw title title.imz
-ConsoleImz -imzw head head.imz
+-ConsoleImz -imzw head head.imz
 -ConsoleImz -imzw item item.imz
 pause
